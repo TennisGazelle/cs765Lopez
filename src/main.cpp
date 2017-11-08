351f83035f8c904314b8e2a0685f6a57e027f62d
Daniel Lopez <daniellopez@nevada.unr.edu>
2017-11-07 21:34:14 -0800

    Added timer, and its usage for examples (look at the C++Template Repo)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <Timer.h>
 
 #include "Logger.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    Timer timer;
+    timer.start();
     Logger logger("../out/somefile.log");
     logger.log("hello world");
+    timer.stop();
+    cout << "writing to file took " << timer.getElapsedTime() << " seconds." << endl;
     return 0;
 }
